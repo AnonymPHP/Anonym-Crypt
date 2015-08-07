5fcda567048114c58538dd592309a669d5baa651
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-07 23:46:58 +0300

    update

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-    /**
-     * Bu Dosya AnonymFramework'e ait bir dosyadır.
-     *
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @see http://gemframework.com
-     *
-     */
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
 
-    include 'vendor/autoload.php';
+include 'vendor/autoload.php';
 
 
 
