00d379107913bfdc33a347041ebe2d91749ec69b
anonymframework <serif.saglam119@outlook.com>
2015-09-15 04:44:40 +0300

    made some updates

--- src/Anonym/Components/SecurityKeyGenerator.php
+++ src/Anonym/Components/SecurityKeyGenerator.php
@@ -46,9 +46,8 @@
          * @return string
          */
         public function random($prefix = ''){
-            $key = $prefix.$this->create();
+            $key = $prefix.$this->create().strval(rand(strlen($_SERVER['REMOTE_ADDR']), rand(111,2222)));
             $uniqid = uniqid($key);
-
             return md5($uniqid);
         }
     }
